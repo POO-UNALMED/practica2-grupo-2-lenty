79a3611bc45eb6dd877a28c4a28c77e9a1f0904f
Camilo <juaczuluagamon@unal.edu.co>
2020-11-24 11:08:10 -0500

    hbox

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -3,6 +3,7 @@ import javafx.application.*;
 import javafx.stage.*;
 import javafx.scene.*;
 import javafx.scene.layout.*;
+import javafx.scene.control.*;
 
 
 
@@ -16,8 +17,23 @@ public class Gui extends Application {
 	public void start(Stage myStage) throws Exception {
 		myStage.setTitle("Lenty");
 		
-		FlowPane rootNode = new FlowPane();
-		Scene myScene = new Scene(rootNode, 600, 400);
+		HBox root = new HBox(10);
+	    VBox vb1 = new VBox(10);
+	    VBox vb2 = new VBox(10);
+	    Label lb1 = new Label("this is p1");
+	    Label lb2 = new Label("this is p2");
+	    
+	    root.setSpacing(230);
+	    
+	    
+	    root.getChildren().add(vb1);
+	    root.getChildren().add(vb2);
+		
+	    vb1.getChildren().add(lb1);
+	    vb2.getChildren().add(lb2);
+		
+
+		Scene myScene = new Scene(root, 600, 400);
 		myStage.setScene(myScene);
 		myStage.show();
 	}	
